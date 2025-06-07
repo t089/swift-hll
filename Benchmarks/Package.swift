36f3689d113f561ca964ceeea40ce0b3f3570763
Tobias Haeberle <tobias.haeberle@me.com>
2025-06-07 10:46:52 +0200

    cleanup

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,27 +1,32 @@
 // swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version: 6.1
+
 import PackageDescription
 
 let package = Package(
-    name: "swift-hyperloglog",
+    name: "benchmarks",
     platforms: [
         .macOS(.v13),
         .iOS(.v16)
     ],
     dependencies: [
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.29.3"),
         .package(path: "../") 
     ],
-    .executableTarget(
-        name: "HyperLogLogBenchmarks",
-        dependencies: [
-            .product(name: "Benchmark", package: "package-benchmark"),
-            "HyperLogLog",
-        ],
-        path: "Benchmarks/HyperLogLogBenchmarks",
-        plugins: [
-            .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
-        ]
-    ),
+    targets: [
+        .executableTarget(
+            name: "HyperLogLogBenchmarks",
+            dependencies: [
+                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "HyperLogLog", package: "swift-hyperloglog"),
+            ],
+            path: "HyperLogLogBenchmarks/",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        ),
+    ]
 )
 
